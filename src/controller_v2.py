67e239395a1c36405b64ecd61e43516e302fede3
Shmulik <shmulike@post.bgu.ac.il>
2020-07-12 10:59:19 +0300

    backup 12.7.20

--- src/controller_v2.py
+++ src/controller_v2.py
@@ -1,15 +1,16 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # license removed for brevity
 # Shmulik Edelman last update 15.6.20
 
 import numpy as np
 import rospy
 from sensor_msgs.msg import Joy
-from std_msgs.msg import Float64, Float32MultiArray
+from std_msgs.msg import Float64, Float64MultiArray
 from geometry_msgs.msg import Twist
 from math import pi, sqrt
 import numpy as np
 import scipy.interpolate as sc  # interp1d, CubicSpline, splprep, splev
+import time
 
 # All dimentions are in mm
 # _dx = 0.05
@@ -20,23 +21,30 @@ _Hz = 100
 _N = 1000
 _joy_dead_zone = 0.12
 _joint_limit = np.deg2rad(40)
-_head_L = 0.2   # meter
+_head_L = 0.2  # meter
+_head_init_x = 160*9
+_head_init_y = 0
+
 
 class Node:
     rospy.init_node(_node_name, anonymous=True)
     rate = rospy.Rate(_Hz)
+
     def reset_sim(self):
-        for i in range(self._link_N+1):
+        for i in range(self._link_N + 1):
             self.pub_vec[i].publish(Float64(data=0))
         self.theta_move = 0
         self.head_angle = 0
         # self.head_move = 0
-        self.A_head = np.asarray([[1, 0, 0],
-                                  [0, 1, 0],
+        self.A_head = np.array([[1, 0, _head_init_x],
+                                  [0, 1, _head_init_y],
                                   [0, 0, 1]])
-        self.path = np.asarray([np.linspace(-self._link_L * self._link_N * 2, 0, self._N, endpoint=True),
-                                np.zeros(self._N),
-                                np.ones(self._N)])
+        # self.path = np.array([np.linspace(-self._link_L * self._link_N * 2, 0, self._N, endpoint=True),
+        #                         np.zeros(self._N),
+        #                         np.ones(self._N)])
+        self.path = np.array([np.linspace(-_head_init_x, _head_init_x, self._N, endpoint=True),
+                              np.zeros(self._N),
+                              np.ones(self._N)])
 
     def __init__(self):
 
@@ -88,63 +96,68 @@ class Node:
 
             self.rate.sleep()
 
-
     def calc_angle(self, joint_pos):
         joint_angles = np.zeros(self._link_N)
         for i in range(1, self._link_N):
             # calculate 2 vectors from 3 points
-            vec1 = (joint_pos[0:2, i] - joint_pos[0:2, i-1])
-            vec2 = (joint_pos[0:2, i+1] - joint_pos[0:2, i])
+            vec1 = (joint_pos[0:2, i] - joint_pos[0:2, i - 1])
+            vec2 = (joint_pos[0:2, i + 1] - joint_pos[0:2, i])
             # calculate the angle between 2 vectors vec1 <-> vec2
-            joint_angles[:, i-1] = np.arctan2(np.cross(vec1, vec2), np.dot(vec1, vec2))
-
+            joint_angles[i - 1] = np.arctan2(np.cross(vec1, vec2), np.dot(vec1, vec2))
         return joint_angles
 
-
-    def split_curve_2(self, points_x, points_y):
+    def split_curve_2(self, path_x, path_y):
         # We will use the 'Bisection method' to find the joint position along the spline
         # https://en.wikipedia.org/wiki/Bisection_method
 
-        counter = 0                                         # count iterations for log
-        epsilon = 1e-3                                      # 0.001 mm
-        joint_pos = np.zeros([self._link_N + 1, 2])     # Create joint positions matrix
+        counter = 0  # count iterations for log
+        epsilon = 1e-3  # 0.001 mm
+        joint_pos = np.zeros([2, self._link_N + 1])  # Create joint positions matrix
 
         # save the position of the true head
-        joint_pos[0, 0] = points_x[-1]
-        joint_pos[1, 0] = points_y[-1]
+        joint_pos[0, 0] = path_x[-1]
+        joint_pos[1, 0] = path_y[-1]
 
         # add head direction points for the spline
         head_dir = np.asarray([[500], [0], [1]])
         p_new = self.A_head * head_dir
-        points_x_new = np.append(points_x, np.linspace(points_x[-1], p_new[0,0], 100))
-        points_y_new = np.append(points_y, np.linspace(points_y[-1], p_new[1, 0], 100))
+        path_x_new = np.append(path_x, np.linspace(path_x[-1] + self._dx / 2, p_new[0, 0], 100))
+        path_y_new = np.append(path_y, np.linspace(path_y[-1], p_new[1, 0], 100))
 
-        # reverse the points to be end->start, so the spline u=0 will be at the end
-        points_x_new = np.fliplr(points_x_new)
-        points_y_new = np.fliplr(points_y_new)
+        # reverse the points to be end->start, so the spline u=0 will be at the head
+        path_x_new = np.flip(path_x_new)
+        path_y_new = np.flip(path_y_new)
+
+        # rospy.loginfo("\npoints X new:\n")
+        # rospy.loginfo(points_x_new)
+        # rospy.loginfo(np.diff(points_x_new))
+        # rospy.loginfo("\npoints Y new:\n")
+        # rospy.loginfo(points_y_new)
+        # rospy.loginfo(np.diff(points_y_new))
+        # points_x_new = np.flip(points_x_new)
+        # points_y_new = np.flip(points_y_new)
 
         # Create spline
-        tck, U = sc.splprep([points_x_new, points_y_new], s=0)
-        # new_points = sc.splev(u_points, tck)
-        # start_points = sc.splev(0, tck)
-        # end_points = sc.splev(1, tck)
+        tck, U = sc.splprep([path_x_new, path_y_new], s=0)
 
         # a, b, c - 3 parameters we use for the 'Bisection method'
         a = c = 0
         b = 1
         u = 1
-        for i in range(self._link_N):
+        for i in range(self._link_N-1):
+            # rospy.loginfo("link {:d}".format(i))
             # reset parameters for each link
             error = error = self._link_L
             a = c
             b = 1
             while error >= epsilon:
+                # rospy.loginfo(error)
                 counter += 1
                 c = (a + b) / 2
-                # p_temp = sc.splev(c, tck)
-                p_temp = np.reshape(sc.splev(c, tck), (2, 1))           # calc temp point along the spline
-                dist = np.linalg.norm(joint_pos[:, i] - p_temp)         # calc the distance from the temp point to the previous joint position
-                error = abs(dist - self._link_L)                        # Update links length error
+                p_temp = np.reshape(sc.splev(c, tck), 2)  # calc temp point along the spline
+                dist = np.linalg.norm(
+                    joint_pos[:, i] - p_temp)  # calc the distance from the temp point to the previous joint position
+                error = abs(dist - self._link_L)  # Update links length error
 
                 # Update a,b,c parameters
                 if dist > self._link_L:
@@ -152,12 +165,12 @@ class Node:
                 else:
                     a = c
 
-            joint_pos[i + 1][:] = p_temp
-            # print('Link #{:d}:\titer: {:d}\terror dist={:.7f}'.format(i + 1, counter, error))
-            # print("Link #", i + 1, "iter", counter, "dist", dist)
-
-        joint_pos = np.fliplr(joint_pos)        # flip the matrix on lef-right so the first joint will be at the beginning of the matrix
-
+            rospy.loginfo(p_temp)
+            joint_pos[:, i + 1] = p_temp
+            time.sleep(0.2)
+        rospy.loginfo(joint_pos)
+        joint_pos = np.fliplr(joint_pos)  # flip the matrix on lef-right so the first joint will be at the beginning of the matrix
+        rospy.loginfo(joint_pos)
         # Calculate the angles at the joints and the first linear rail distance
         # First angle
         vec1 = np.array([[1], [0]])
@@ -165,24 +178,19 @@ class Node:
         # angle = np.math.acos(np.dot(v0, v1) / (np.linalg.norm(v0) * np.linalg.norm(v1)))
         first_angle = np.math.atan2(vec2[1], vec2[0]) - np.math.atan2(vec1[1], vec1[0])
 
-        joint_angles = np.asarray([first_angle])
+        joint_angles = np.array([first_angle])
         joint_angles = np.append(joint_angles, self.calc_angle(joint_pos))
 
-        joint_cmd = np.array([joint_pos[0, 0], joint_angles])
+        joint_cmd = np.append(joint_pos[0, 0]*(1e-3), first_angle)      # (1e-3) convert from mm to meter
+        joint_cmd = np.append(joint_cmd, joint_angles)
+        rospy.loginfo("joint_cmd: {:.3f}".format(joint_cmd[0]))
 
-        # Print log to terminal
-        msg_info = "Average iterations per link = {.1d}".format(counter/self._link_N)
-        rospy.loginfo(msg_info)
 
         return joint_cmd
 
-
-
-
-
     def split_curve(self, points_x, points_y, link_N, link_L):
         counter = 0
-        epsilon = 1e-2      # Allowed error = 0.01 mm
+        epsilon = 1e-2  # Allowed error = 0.01 mm
         e = link_L
         px0 = points_x[-1]
         py0 = points_y[-1]
@@ -222,8 +230,8 @@ class Node:
             angle = np.arctan2(np.cross(v0, v1), np.dot(v0, v1))
             joints_angles = np.concatenate((joints_angles, [angle]))
 
-        joints_points = (joints_points + 1600)/1000
-        return joints_points[:,0], joints_angles, tck
+        joints_points = (joints_points + 1600) / 1000
+        return joints_points[:, 0], joints_angles, tck
 
     def stretch(self, x, in_min, in_max, out_min, out_max):
         x, in_min, in_max, out_min, out_max = map(float, (x, in_min, in_max, out_min, out_max))
@@ -235,8 +243,8 @@ class Node:
                            [0, 0, 1]])
 
     def head_turn(self, state):
-        if abs(state)>_joy_dead_zone and abs(self.head_angle+self._dtheta*(-state)) < _joint_limit:
-            self.theta_move = self._dtheta*(-state)
+        if abs(state) > _joy_dead_zone and abs(self.head_angle + self._dtheta * (-state)) < _joint_limit:
+            self.theta_move = self._dtheta * (-state)
             self.head_angle += self.theta_move
             vec_view = [[500], [0], [1]]
             # self.R_z = self.Rz(0, self.theta_move)
@@ -245,16 +253,6 @@ class Node:
             # Start section print and plot of the snake:
             # Remove old plot elements
 
-            # --- For plot:
-            # self.ax.lines.remove(self.head_dir)
-            # self.head_dir, = plt.plot([self.head_point_new[0], head_direction[0]],
-            #                           [self.head_point_new[1], head_direction[1]], 'r-')
-            # plt.draw()
-            # plt.pause(0.00000000001)
-            # plt.axis('equal')
-            # plt.axis([-1700, 4000, -20, 20])
-            # End section print & Plot
-            # print("Head angle: {:.3f}".format(self.head_angle))
             self.pub_vec[self._link_N].publish(Float64(data=self.head_angle))
 
     def joystick_Left_X(self, state):
@@ -266,64 +264,32 @@ class Node:
             x_move = self._dx * state
             vec0 = [[0], [0], [1]]
             vec_view = [[500], [0], [1]]
-            self.R_z = np.asarray([[np.cos(self.theta_move), -np.sin(self.theta_move), np.cos(self.theta_move) * x_move],
-                                   [np.sin(self.theta_move), np.cos(self.theta_move), np.sin(self.theta_move) * x_move],
-                                   [0, 0, 1]])
-            self.A_head = np.dot(self.A_head, self.R_z)             # Update A_hed matrix
+            self.R_z = np.array(
+                [[np.cos(self.theta_move), -np.sin(self.theta_move), np.cos(self.theta_move) * x_move],
+                 [np.sin(self.theta_move), np.cos(self.theta_move), np.sin(self.theta_move) * x_move],
+                 [0, 0, 1]])
+            self.A_head = np.dot(self.A_head, self.R_z)  # Update A_hed matrix
 
-            self.head_point_new = np.dot(self.A_head, vec0)              # Get new head position
+            self.head_point_new = np.dot(self.A_head, vec0)  # Get new head position
             # head_direction = np.dot(self.A_head, vec_view)          # Get new head Direction
+
+
 
             self.path = np.concatenate((self.path, self.head_point_new), axis=1)  # Add new head position to path
             x = np.squeeze(np.asarray(self.path[0, :]))
             y = np.squeeze(np.asarray(self.path[1, :]))
-            # joints_points, joints_angles, tck = self.split_curve(x, y, self._link_N, self._link_L)
+
             joint_cmd = self.split_curve_2(x, y)
             self.theta_move = 0
 
 
-            # Start section print and plot of the snake:
-            # Remove old plot elements
-            # --- For plot:
-            # self.ax.lines.remove(self.head_dir)
-            # self.ax.lines.remove(self.links_state)
-
-            # Print the angles in console:
-            # for angle_i in joints_angles:
-            #     print("{:.2f}".format(np.rad2deg(angle_i)), end=" ")
-            # for angle_i in range(len(joints_angles)):
-            #     print("{:.2f}".format(np.rad2deg(joints_angles[angle_i]))),
-                # print("{:.2f}".format(np.rad2deg(joints_angles[angle_i])), end = angle_i == (len(joints_angles)-1) ? " " : "\n";)
-                # print("{:.2f}".format(np.rad2deg(joints_angles[angle_i])),
-                #       end = " " if angle_i < (len(joints_angles) - 1) else "\n")
-            # print('.')
-
             print("Origin pos: {:.3f}".format(joint_cmd[0]))
-            for i in range(self._link_N+1):
-                pub_vec[i].publish(Float64(data=joints_angles[i]))
+            rospy.loginfo(joint_cmd)
+            rospy.loginfo(joint_cmd.shape)
+            for i in range(self._link_N + 1):
+                rospy.loginfo("joint {:d}\t{:.3f}".format(i, joint_cmd[i]))
+                self.pub_vec[i].publish(Float64(data=joint_cmd[i]))
 
-
-            # print("Origin pos: {:.3f}".format(joints_points[0])),
-            # self.pub_vec[0].publish(Float64(data=joints_points[0]))
-            # for i in range(self._link_N):
-            #     self.joint_vals.data[i+1] = joints_angles[i]
-                # print("{:.2f}".format(joints_angles[i])),
-                # print("Joint {} angle: {:.3f}".format(i, joints_angles[i]))
-                # self.pub_vec[i+1].publish(Float64(data=joints_angles[i]))
-            # self.head_angle = self.head_angle - joints_angles[-1]
-            # self.pub_vec[self._link_N].publish(Float64(data=self.head_angle))
-            # print(".")
-            # --- Plot the snake head direction
-            #self.head_dir, = plt.plot([self.head_point_new[0], head_direction[0]], [self.head_point_new[1], head_direction[1]], 'r-')
-            # Plot the snake links and joints
-            # self.links_state, = plt.plot(joints_points[:, 0], joints_points[:, 1], 'og-', markersize=2)
-            # plt.plot([self.head_point_new[0]], [self.head_point_new[1]], 'm.', markersize=2)
-            ### self.arc_state, = plt.plot([-1600, joints_points[0, 0]], [0, joints_points[0, 1]], 'm--', markersize=2)
-            # plt.draw()
-            # plt.pause(0.00000000001)
-            # plt.axis('equal')
-            # plt.axis([-1700, 4000, -20, 20])
-            # End section print & Plot
 
     def joy_update(self, data):
         """callback for /joy
@@ -342,8 +308,8 @@ class Node:
             self.reset_sim()
         # if pygame.joystick.Joystick(joy).get_button(0) == 1:
         #     run = False
-        #self.x_move = _dx * data.axes[1]
-        #self.theta_move = _dTheta * data.axes[0]
+        # self.x_move = _dx * data.axes[1]
+        # self.theta_move = _dTheta * data.axes[0]
 
 
 if __name__ == '__main__':
